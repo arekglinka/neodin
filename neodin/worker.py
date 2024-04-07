490bfcf07d8f4e9329d4cd59317c4c7dba08d08c
ArekGlinka <arekglinka@gmail.com>
2024-04-07 20:59:31 +0200

    wip

--- neodin/worker.py
+++ neodin/worker.py
@@ -10,11 +10,13 @@ import time
 import fcntl
 from typing import Callable
 import re
-from typing import Generic, TypeVar
+from typing import ClassVar, Iterable
 import traceback
-
+import zoneinfo
 import ruamel.yaml as yaml
-
+from hashlib import md5
+import logging
+import logging
 
 
 FORMAT = '[%(asctime)s] %(message)s'
@@ -62,68 +64,63 @@ class StateLocationConfig(BaseModel):
         self.results_dir.mkdir(parents=True, exist_ok=True)
 
 
+class YamlSerde(BaseModel):
+    def dump(self, path: Path) -> None:
+        with path.open("w") as f:
+            f.write(yaml.safe_dump(self.model_dump()))
 
 
-class AbstractJobConfig(BaseModel):
+    @classmethod
+    def load(cls, path: Path) -> dict:
+        return cls(**yaml.safe_load(path.read_text()))
+
+class JobConfig(BaseModel):
     state_location: StateLocationConfig
-    keep_retrying: bool = False
     dry_run: bool = False
     command: str
-    variable_job_args: dict[str, str] = {}
-    constant_job_args: dict[str, str] = {}
+    env: dict[str, str] = {}
+    constant_args: dict[str, str] = {}
+    variable_args: list[dict[str, str]] = []
+    keep_retrying: bool = False
+
+    job_filename_regex: ClassVar[re.Pattern] = re.compile(r"^(?P<job_class_name>.+)_(?P<var_job_args_hash>.+)\.(?P<extension>\w+)$")
+    job_instance_filename_regex: ClassVar[re.Pattern] = re.compile(r"^(?P<job_class_name>.+)_(?P<var_job_args_hash>.+)_(?P<timestamp>w+)\.(?P<extension>\w+)$")
     
-    @classmethod
-    def is_job_file(cls, job_file_path: Path) -> bool:
-        return job_file_path.suffix == '.job'
 
-    @property
-    def job_id(self) -> str:
-        return re.sub(r'\W+', '-', self.name_format)
+    job_name_format: ClassVar[str] = "{job_class_name}_{var_job_args_hash}"
+    job_extension: ClassVar[str] = ".job.yaml"
 
-    @property
-    def name_format(self) -> str:
-        job_class_name = self.__class__.__name__
-        var_job_args = "-".join(f"{name}_{value}" for name, value in self.variable_job_args.items())
-        return f"{job_class_name}_{var_job_args}.job"
+    job_instance_name_format: ClassVar[str] = "{job_class_name}_{var_job_args_hash}_{timestamp}"
+    job_instance_extension: ClassVar[str] = "jobinstance.yaml"
+    job_instance_result_extension: ClassVar[str] = "result.yaml"
+    job_instance_log_extension: ClassVar[str] = "log.txt"
+    
 
-    def create_job_file(self, job_id: str, jobs_path: Path) -> AbstractJobConfig:
-        job_file_path = jobs_path / f"{job_id}.job"
-        with job_file_path.open("w") as job_file:
-            yaml.dump(self.dict(), job_file)
-        return self
 
-    def get_subprocess_command(self) -> SubprocessCommand:
-        return SubprocessCommand(command=self.command, args=[f"{name}={value}" for name, value in (self.constant_job_args | self.variable_job_args).items()])
+    def list_jobs(self) -> list[Path]:
+        return [job_file_path for job_file_path in state_location.jobs_dir.glob("*.job.yaml") if self.is_job_file(job_file_path)]
+    
+    def is_job_file(self, file_path: Path) -> bool:
+        return self.job_filename_regex.fullmatch(file_path.name) is not None
+    
+    def var_job_args_hash(self, variable_args: dict[str, str]) -> str:
+        return md5(str(variable_args).encode()).hexdigest()
 
-    @classmethod
-    def from_job_file(cls, job_file_path: Path) -> AbstractJobConfig:
-        with job_file_path.open("r") as job_file:
-            job = yaml.safe_load(job_file)
-        return cls(**job)
+    def create_jobs(self) -> Iterable[Job]:
+        for variable_args in self.variable_args:
+            yield Job(
+                job_class_name=self.__class__.__name__,
+                var_job_args=variable_args,
+                command=self.get_subprocess_command(variable_args),
+            )
 
-    def execute(self) -> int:
-        logger.debug(f'STARTING: {self.job_id}')
-        started_at = dt.datetime.utcnow()
-        self.state_location.make_dirs()
-        success = False
-        log_file_path = self.state_location.logs_dir / f"{self.job_id}.log"
-        try:
-            with log_file_path.open("w") as log_file:
-                retcode = self.get_subprocess_command().execute(self.dry_run, log_file)
-                success = retcode == 0
-                return success
-        except Exception as e:
-            ...
-        finally:
-            logger.debug(f'FINISHED: {self.job_id}, success={success}')
-            with (self.state_location.results_dir / f"{self.job_id}.result").open("w") as result_file:
-                yaml.dump(dict(
-                    job_id=self.job_id,
-                    success=success,
-                    started_at=started_at.isoformat(),
-                    log=log_file_path.as_posix(),
-                    run_time_seconds=(dt.datetime.utcnow() - started_at).total_seconds()
-                ), result_file)
+    def crete_job_files(self) -> None:
+        for job in self.create_jobs():
+            job.create_job_file(jobs_path=self.state_location.jobs_dir)
+
+
+    def get_subprocess_command(self, variable_args) -> SubprocessCommand:
+        return SubprocessCommand(command=self.command, args=[f"{name}={value}" for name, value in (self.constant_args | variable_args).items()])
 
 
 class SubprocessCommand(BaseModel):
@@ -140,22 +137,97 @@ class SubprocessCommand(BaseModel):
             process.communicate()
             return process.returncode
 
+class Job(YamlSerde):
+    # __mro__: list[type] = [BaseModel, YamlSerde]
+    job_class_name: str
+    var_job_args: dict[str, str]
+    command: SubprocessCommand
 
-T = TypeVar('T', bound=AbstractJobConfig)
+    def get_job_config(self) -> JobConfig:
+        # return globals()[self.job_class_name]()
+        return globals()["job_config"]
+    
+    def create_instance(self, dry_run: bool = False, timestamp_provider: Callable[[], dt.datetime] = dt.datetime.now) -> JobInstance:
 
+        job_config = self.get_job_config()
+        job_name = job_config.job_instance_name_format.format(
+            job_class_name=self.__class__.__name__, 
+            var_job_args_hash=job_config.var_job_args_hash(variable_args=self.var_job_args), 
+            timestamp=dt.datetime.now().isoformat()
+        )
 
-class JobFactory(BaseModel, Generic[T]):
-    job_class: type[AbstractJobConfig]
-    command: str
-    constant_job_args: dict[str, str] = {}
-    variable_job_args: list[dict[str, str]] = []
+        return JobInstance(
+            log_file_path_str=(job_config.state_location.logs_dir / (job_name + job_config.job_instance_log_extension)).as_posix(),
+            results_file_path_str=(job_config.state_location.results_dir / (job_name + job_config.job_instance_result_extension)).as_posix(),
+            job=self,
+            dry_run=dry_run,
+            timestamp=timestamp_provider(),
+        )
+    def create_job_file(self, jobs_path: Path) -> None:
+        job_config = self.get_job_config()
+        job_file_path = jobs_path / (job_config.job_name_format.format(
+            job_class_name=self.job_class_name,
+            var_job_args_hash=job_config.var_job_args_hash(variable_args=self.var_job_args)
+        ) + job_config.job_extension)
+        with job_file_path.open("w") as job_file:
+            job_file.write(yaml.safe_dump(self.model_dump()))
+        return job_file_path
 
-    def create_job(self, job_id: str, **kwargs) -> T:
-        return self.job_class(state_location=self.constant_job_args, variable_job_args=kwargs)
+class JobInstance(YamlSerde):
+    # __mro__: list[type] = [BaseModel, YamlSerde]
+    log_file_path_str: str
+    results_file_path_str: str
+    job: Job
+    dry_run: bool = False
+    timestamp: dt.datetime = dt.datetime.now()
+    
 
+    def dump(self, jobs_path: Path) -> None:
+        content = yaml.safe_dump(self.model_dump())
+        job_config_class = self.job.get_job_config_class()
+        job_file_path = jobs_path / job_config_class.job_instance_name_format.format(
+            job_class_name=self.job.job_class_name,
+            var_job_args_hash=self.job.var_job_args,
+            timestamp=self.timestamp.isoformat()
+        )
+        
+        with job_file_path.open("w") as job_file:
+            job_file.write(content)
+        return self
 
-def list_jobs(state_location: StateLocationConfig, job_class: type[AbstractJobConfig]) -> list[Path]:
-    return [job_file_path for job_file_path in state_location.jobs_dir.glob("*.job") if job_class.is_job_file(job_file_path)]
+    @classmethod
+    def from_job_file(cls, job_file_path: Path) -> JobInstance:
+        with job_file_path.open("r") as job_file:
+            content = job_file.read()
+        job = yaml.safe_load(content)
+        return cls(**job)
+
+    def execute(self) -> int:
+        logger = logging.getLogger(__name__)
+
+        logger.debug(f'STARTING: {self.job.var_job_args}')
+        started_at = dt.datetime.now()
+        success = False
+        log_file_path = Path(self.log_file_path_str)
+        result_file_path = Path(self.results_file_path_str)
+        try:
+            with log_file_path.open("w") as log_file:
+                retcode = self.job.command.execute(self.dry_run, log_file)
+                success = retcode == 0
+                return success
+        except Exception as e:
+            logger.error(f'Error: {e}', exc_info=e)
+        finally:
+            logger.debug(f'FINISHED: {self.job.var_job_args}, success={success}')
+            with result_file_path.open("w") as result_file:
+                yaml.dump(dict(
+                    command=self.job.command,
+                    success=success,
+                    started_at=started_at.isoformat(),
+                    log=log_file_path.as_posix(),
+                    run_time_seconds=(dt.datetime.utcnow() - started_at).total_seconds()
+                ), result_file)
+
 
 
 class JobAlreadyRunningError(Exception):
@@ -166,79 +238,81 @@ class JobFinishingError(Exception):
     ...
 
 
-def unlock_and_move_job_file(job_path: Path, config: AbstractJobConfig):
-    job = config.from_job_file(job_path)
+def unlock_and_move_job_file(job_path: Path, config: JobConfig, success: bool):
     try:
         with open(job_path, "w") as job_file:
             fcntl.flock(job_file, fcntl.LOCK_UN)
 
-        if job.success:
-            job_path.rename(job.state_location.done_dir / job_path.name)
+        if success:
+            job_path.rename(config.state_location.done_dir / job_path.name)
         else:
-            job_path.rename(job.state_location.failed_dir / job_path.name)
+            job_path.rename(config.state_location.failed_dir / job_path.name)
     except Exception as e:
         logger.error(f'Error: {e}', exc_info=e)
         raise JobFinishingError(f"Error finishing job {job_path}")
 
 
 class JobWorker:
-    def __init__(self, state_location: StateLocationConfig, job_class: type[AbstractJobConfig]):
+    def __init__(self, state_location: StateLocationConfig, job_config: JobConfig):
         self.state_location = state_location
-        self.job_class = job_class
+        self.job_config = job_config
 
-    def manage_job_locks(self, job_path: Path, callback: Callable[[], bool], config: AbstractJobConfig):
+    def manage_job_locks(self, job_path: Path, callback: Callable[[], bool]):
         try:
             success = False
+
             with open(job_path, "w") as job_file:
                 fcntl.flock(job_file, fcntl.LOCK_EX | fcntl.LOCK_NB)
                 success = callback()
-            if success or not self.job_class.keep_retrying:
-                unlock_and_move_job_file(job_path, config=config)
+            if success or not self.job_config.keep_retrying:
+                unlock_and_move_job_file(job_path, config=self.job_config, success=success)
         except BlockingIOError:
             logger.debug(f'Job already running: {job_path}')
             traceback.print_exc()
+
             raise JobAlreadyRunningError(f"Attempted to run job {job_path} but it is already running.")
 
     def start_worker(self):
         while True:
-            jobs_todo = list_jobs(self.state_location, self.job_class)
+            jobs_todo = self.job_config.list_jobs()
             if not jobs_todo:
                 logger.warning('No jobs to do. Exiting.')
                 exit(0)
             try:
                 job_file_path = random.choice(jobs_todo)
+
             except Exception as e:
                 logger.error(f'Error: {e}')
 
             
             try:
-                job = self.job_class.from_job_file(job_file_path)
-                job.execute()
+                job = Job.load(job_file_path)
+                instance = job.create_instance()
+                self.manage_job_locks(job_path=job_file_path,callback=lambda: instance.execute())
             except JobAlreadyRunningError:
                 logger.debug(f'Job already running: {job_file_path}')
                 time.sleep(1)
 
+class ConcreteJobConfig(JobConfig):
+    env_type: str
 
-
+state_location = StateLocationConfig.user_home_relative('test')
+state_location.make_dirs()
+job_config = ConcreteJobConfig(env_type='prod',
+    command="echo", 
+    state_location=state_location,
+    constant_args=dict(to_print='dupa'), 
+    variable_args=[
+        dict(aaa='1'),
+        dict(aaa='2')
+    ],
+)
 
 
 if __name__ == '__main__':
-    class ConcreteJobConfig(AbstractJobConfig):
-        env_type: str
-    
-    factory = JobFactory(
-        command="echo", 
-        job_class=ConcreteJobConfig, 
-        constant_job_args=["to_print='dupa'"], 
-        variable_job_args=[
-            dict(aaa=1),
-            dict(aaa=2)
-        ],
-    )
-    factory.create_job('test')
 
-    state_location = StateLocationConfig.user_home_relative('test')
-    state_location.make_dirs()
-    job_class = ConcreteJobConfig
-    worker = JobWorker(state_location, job_class)
+
+    job_config.crete_job_files()
+
+    worker = JobWorker(state_location=state_location, job_config=job_config)
     worker.start_worker()
